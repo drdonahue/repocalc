6cee9dd54d2f2b1161adbd694c6d7d9aeff31d04
David Donahue <david@donahue6.us>
2019-05-29 15:01:01 -0600

    Implemented nc_draw_stack to display the stack to the user

--- ui.c
+++ ui.c
@@ -14,16 +14,21 @@ void nc_loop (void)
 {
     int exit = 0;
     int c;
+    stk_elem * stack = NULL;
+
     nc_draw_screen();
     while (!exit)
     {
         c = getch();
         exit = ( c == K_EXIT );
+        /* Redraw the screen if the terminal is resized */
         if (c == KEY_RESIZE)
         {
             nc_draw_screen();
+            nc_draw_stack(stack);
         }
     }
+    stk_free(&stack);
 }
 
 void nc_draw_screen (void)
@@ -35,13 +40,14 @@ void nc_draw_screen (void)
     getmaxyx(stdscr, h, w);
 
     /* create a blank line for the last 3 lines of the screen */
-    blankline = (char *) malloc(sizeof(char) * w);
+    blankline = (char *) malloc(sizeof(char) * w+1);
 
     for (i = 0; i < w-1; ++i)
     {
         blankline[i] = ' ';
     }
     blankline[w-1] = '\n';
+    blankline[w] = 0;
 
     /* Go to 0,0 on the screen */
     move(0,0);
@@ -64,6 +70,38 @@ void nc_draw_screen (void)
 
     free(blankline);
 
+}
+void nc_draw_stack (const stk_elem * stack)
+{
+    int h,w,i,stack_size;
+    char * blankline;
+
+    getmaxyx(stdscr, h, w);
+
+    /* create a blank line for empty stack elements */
+    blankline = (char *) malloc(sizeof(char) * w-5);
+
+    for (i = 0; i < w-7; ++i)
+    {
+        blankline[i] = ' ';
+    }
+    blankline[w-7] = '\n';   
+    blankline[w-6] = 0;
+
+    stack_size = stk_size(stack);
+
+    for (i = 0; i < h-4; ++i)
+    {
+        move(h-(4+i), 5);
+        if (i < stack_size)
+            wprintw(stdscr, "%g", stk_at(stack, i));
+        else
+            wprintw(stdscr, blankline);
+    }
+    
+
+
+    free(blankline);
 }
 
 void nc_exit (void)
--- ui.h
+++ ui.h
@@ -4,10 +4,12 @@
 #include <curses.h>
 #include <stdlib.h>
 #include "config.h"
+#include "stack.h"
 
 void nc_init (void);
 void nc_loop (void);
 void nc_draw_screen(void);
+void nc_draw_stack (const stk_elem * stack);
 void nc_exit (void);
 
 #endif
