1e2c669b892ec7193273904564ad1105b4375835
David Donahue <david@donahue6.us>
2020-01-12 13:55:55 -0600

    Fixed customfuncs.def.x doccumentation

--- customfuncs.def.h
+++ customfuncs.def.h
@@ -7,7 +7,7 @@
 /* Place the prototypes for any custom functions in this file
  * Custom functions must be void and accept 3 arguments:
  * stk_elem **      stack       The stack. See stack.h for stack functions
- * double           input       The number on the input line. Check if it is NAN before using
+ * double *         input       The number on the input line. Check if it is NAN before using
  * unsigned int *   cursorpos   Pointer to the current postision of the cursor. 
  * const Arg *      arg         struct that allows for a special argument to be passed. Set in config.h, add the argument to the struct.
  */
